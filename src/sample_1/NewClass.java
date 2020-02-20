da74454816914670dbe054a2c113b0e7bff77d2c
kaur6869 <kaur6869@sheridancollege.ca>
2020-02-20 08:16:46 -0500

    first commit

---
+++ src/sample_1/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample_1;
+
+/**
+ *
+ * @author Gurpreet
+ */
+public class NewClass {
+    private int a;
+    private double b;
+    private String s;
+    
+}
